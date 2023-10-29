b67edf8284c85fda622ef8e5bcc49032bbedb1e3
Rafael Gonçalves <rafaelswitek@gmail.com>
2023-10-29 16:06:34 -0300

    03. Rota de livros favoritos

--- controladores/livro.js
+++ controladores/livro.js
@@ -7,30 +7,30 @@ function getLivros(req, res) {
     } catch (error) {
         res.status(500)
         res.send(error.message)
-    }
+    } 
 }
 
 function getLivro(req, res) {
     try {
         const id = req.params.id
 
-        if (id && Number(id)) {
+        if(id && Number(id)) {
             const livro = getLivroPorId(id)
             res.send(livro)
         } else {
             res.status(422)
-            res.send('ID inválido')
+            res.send("Id inválido")
         }
     } catch (error) {
         res.status(500)
         res.send(error.message)
-    }
+    } 
 }
 
 function postLivro(req, res) {
     try {
         const livroNovo = req.body
-        if (req.body.nome) {
+        if(req.body.nome) {
             insereLivro(livroNovo)
             res.status(201)
             res.send("Livro inserido com sucesso")
@@ -38,8 +38,8 @@ function postLivro(req, res) {
             res.status(422)
             res.send("O campo nome é obrigatório")
         }
-
-    } catch (error) {
+        
+    } catch(error) {
         res.status(500)
         res.send(error.message)
     }
@@ -49,37 +49,36 @@ function patchLivro(req, res) {
     try {
         const id = req.params.id
 
-        if (id && Number(id)) {
+        if(id && Number(id)) {
             const body = req.body
-
             modificaLivro(body, id)
             res.send("Item modificado com sucesso")
         } else {
             res.status(422)
-            res.send('ID inválido')
+            res.send("Id inválido")
         }
-
-    } catch (error) {
+       
+    } catch(error) {
         res.status(500)
-        res.send(error.message)
+        res.send(error.message) 
     }
 }
 
 function deleteLivro(req, res) {
     try {
         const id = req.params.id
-        if (id && Number(id)) {
+
+        if(id && Number(id)) {
             deletaLivroPorId(id)
             res.send("livro deletado com sucesso")
         } else {
             res.status(422)
-            res.send('ID inválido')
+            res.send("ID inválido")
         }
-
     } catch (error) {
         res.status(500)
         res.send(error.message)
-    }
+    } 
 }
 
 module.exports = {
--- servicos/livro.js
+++ servicos/livro.js
@@ -1,20 +1,20 @@
 const fs = require("fs")
 
 function getTodosLivros() {
-    return JSON.parse(fs.readFileSync("livros.json"))
+    return JSON.parse( fs.readFileSync("livros.json") )
 }
 
 function getLivroPorId(id) {
     const livros = JSON.parse(fs.readFileSync("livros.json"))
 
-    const livroFiltrado = livros.filter(livro => livro.id === id)[0]
+    const livroFiltrado = livros.filter( livro => livro.id === id )[0]
     return livroFiltrado
 }
 
 function insereLivro(livroNovo) {
     const livros = JSON.parse(fs.readFileSync("livros.json"))
 
-    const novaListaDeLivros = [...livros, livroNovo]
+    const novaListaDeLivros = [ ...livros, livroNovo ]
 
     fs.writeFileSync("livros.json", JSON.stringify(novaListaDeLivros))
 }
@@ -26,13 +26,14 @@ function modificaLivro(modificacoes, id) {
     const conteudoMudado = { ...livrosAtuais[indiceModificado], ...modificacoes }
 
     livrosAtuais[indiceModificado] = conteudoMudado
+
     fs.writeFileSync("livros.json", JSON.stringify(livrosAtuais))
 }
 
 function deletaLivroPorId(id) {
     const livros = JSON.parse(fs.readFileSync("livros.json"))
 
-    const livrosFiltrados = livros.filter(livro => livro.id !== id)
+    const livrosFiltrados = livros.filter( livro => livro.id !== id )
     fs.writeFileSync("livros.json", JSON.stringify(livrosFiltrados))
 }
 
