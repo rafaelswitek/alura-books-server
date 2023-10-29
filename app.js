a38b37771897ca89778619989922753576310413
Rafael Gonçalves <rafaelswitek@gmail.com>
2023-10-29 15:28:54 -0300

    02. Requisições com Axios

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express")
+const cors = require("cors")
 const rotaLivro = require("./rotas/livro")
 
 const app = express()
 app.use(express.json())
+app.use(cors({ origin: "*" }))
 
 app.use('/livros', rotaLivro)
 
